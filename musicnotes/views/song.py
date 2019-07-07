9967e5a5526b91395e241b6514ea6fb1d369b662
Oscar de Groot <oscar.degroot@hotmail.com>
2019-07-07 22:45:54 +0200

    Fixed django 2.0 reference

--- musicnotes/views/song.py
+++ musicnotes/views/song.py
@@ -1,7 +1,7 @@
 from django.views.generic.edit import CreateView, UpdateView
 from .base import LoginRequiredMixin
 from ..forms import SongForm, InstrumentForm
-from django.core.urlresolvers import reverse
+from django.url import reverse
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect, HttpResponseRedirect
